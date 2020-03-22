b497a942e1c517221daf53fc81cefa91a756921a
hafiizhekom <hafiizhekom@yahoo.com>
2020-03-23 06:03:38 +0700

    Get Recipes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 
 import React, { Component } from 'react';
 import axios from 'axios'
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   constructor(props) { 
@@ -11,10 +9,13 @@ class App extends Component {
 
     this.state = {
       data_ingredient:[],
-      data_checkbox:[]
+      data_checkbox:[],
+      data_recipes:[],
+      date:""
     }
 
     this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
   }
 
   async componentDidMount(){
@@ -31,7 +32,6 @@ class App extends Component {
     .then(function (response) {
       
       var data = JSON.stringify(response.data).split("use-by").join("useby");
-      console.log(data);
       data = JSON.parse(data);
       var data_checkbox = new Array(data.length).fill(false);
       self.setState({data_ingredient:data, data_checkbox:data_checkbox})
@@ -42,6 +42,10 @@ class App extends Component {
   }
 
   handleCheckboxChange(e){
+    if(this.state.date===""){
+      this.setDefaultDate();
+    }
+
     var current_checkbox = this.state.data_checkbox;
     if(e.target.checked){
       current_checkbox[e.target.id] = true;
@@ -58,11 +62,26 @@ class App extends Component {
         keyword = keyword.concat(ingredient.title.toString(),",");
         
       }
+      return null
     })
+
+    keyword = keyword.substr(0,keyword.length-1);
+
     this.getRecipes(keyword);
   }
 
+  setDefaultDate(){
+      var default_date = new Date();
+      default_date = default_date.getFullYear()+'-'+("0" + (default_date.getMonth() + 1)).slice(-2)+'-'+("0" + default_date.getDate()).slice(-2);
+      this.setState({date:default_date});
+  }
+
+  handleDateChange(e){
+    this.setState({date:e.target.value});
+  }
+
   getRecipes(ingredients){
+    var self = this;
     axios.get(global.Server+'recipes', 
     { 
       params:{ingredients:ingredients},
@@ -70,11 +89,11 @@ class App extends Component {
     )
     .then(function (response) {
       
-      console.log(response.data);
+      self.setState({data_recipes:response.data});
       
     })
     .catch(function (error) {
-      
+      self.setState({data_recipes:[]});
     });
   }
 
@@ -82,31 +101,15 @@ class App extends Component {
   render(){
     return (
       <div>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
 
           <div className="container">
-            <h1>Makan</h1>
+            <h2>Lunch Recipes Suggestion</h2>
             <form>
               <div className="form-group">
                 <label>Date:</label>
-                <input type="date" className="form-control" required/>
+                <input type="date" className="form-control" required onChange={this.handleDateChange} value={this.state.date}/>
               </div>
-              <label>Date:</label>
+              <label>Ingredients:</label>
               <div className="form-check">
 
                 {
@@ -127,14 +130,48 @@ class App extends Component {
                               </div>
                             )
                   })
-                  : <p>a{this.state.data_ingredient}</p>
+                  : null
                 }
-                <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
-                <label className="form-check-label" htmlFor="defaultCheck1">
-                  Default checkbox
-                </label>
               </div>
             </form>
+            
+            
+              
+                  {
+                    this.state.data_recipes.length>0
+                    ?
+                    <div className="result" style={{marginTop:20}}>
+                      <h3>Recipes</h3>
+                      <div className="row" >
+                        {
+                          this.state.data_recipes.map((recipe,index) => {
+                              return (
+                                <div className="col-4" key={index}>
+                                  <div className="card">
+                                    <div className="card-body">
+                                      <h4 className="card-title">{recipe.title}</h4>
+                                      <ul className="list-group">
+                                        {
+                                          recipe.ingredients.map((ingredient, index) =>{
+                                            return (
+                                              <li className="list-group-item" key={index}>{ingredient}</li>
+                                            )
+                                          })
+                                        }
+                                      </ul>
+                                    </div>
+                                  </div>
+                                </div>
+                              );
+                          })
+                        }
+                      </div>
+                    </div>
+                    :
+                    null
+                  }
+              
+            
           </div>
       </div>
     );
